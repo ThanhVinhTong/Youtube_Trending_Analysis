0f29671f8f0c48133ec8e009178d1820fa1b8ec9
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-01-11 08:28:31 +0700

    update data

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
 	elif sys.argv[1] == '2':
 		visualize.plot_2(data)
 	elif sys.argv[1] == '3':
+		# TODO
 		pass
 	elif sys.argv[1] == '4':
+		# TODO
 		pass
 	elif sys.argv[1] == '5':
 		visualize.plot_5(data)
-	elif sys.argv[1] == '6':
-		pass
 	else:
 		print('No plot code selected')
