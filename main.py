8885e5a7ca2d27027f6946e0406cc7155e0c0ca3
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-01-31 01:37:53 +0700

    Update Project's EDA

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, crawl, merge_files, visualize
+import sys, crawl, merge_files
 
 if __name__ == '__main__':
 	# crawl
@@ -8,19 +8,3 @@ if __name__ == '__main__':
 	# merge files
 	if '--merge_files' in sys.argv:
 		merge_files.main()
-
-	# visualize
-	if sys.argv[1] == '1':
-		data = merge_files.read()
-		visualize.plot_1(data)
-	elif sys.argv[1] == '2':
-		data = merge_files.read()
-		visualize.plot_2(data)
-	elif sys.argv[1] == '3':
-		data = merge_files.read()
-		visualize.plot_3(data)
-	elif sys.argv[1] == '4':
-		data = merge_files.read()
-		visualize.plot_4(data)
-	else:
-		print('No plot code selected')
