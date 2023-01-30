bc75f4d441e5e4817186fe0b82c8b2fb5b9387c5
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-01-30 23:07:37 +0700

    change clean.py to merge_files.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-import sys, crawl, clean, visualize
+import sys, crawl, merge_files, visualize
 
 if __name__ == '__main__':
 	# crawl
 	if '--crawl' in sys.argv:
 		crawl.main()
 
-	# clean
-	if '--clean' in sys.argv:
-		clean.main()
+	# merge files
+	if '--merge_files' in sys.argv:
+		merge_files.main()
 
 	# visualize
 	if sys.argv[1] == '1':
-		data = clean.read()
+		data = merge_files.read()
 		visualize.plot_1(data)
 	elif sys.argv[1] == '2':
-		data = clean.read()
+		data = merge_files.read()
 		visualize.plot_2(data)
 	elif sys.argv[1] == '3':
-		data = clean.read()
+		data = merge_files.read()
 		visualize.plot_3(data)
 	elif sys.argv[1] == '4':
-		data = clean.read()
+		data = merge_files.read()
 		visualize.plot_4(data)
 	else:
 		print('No plot code selected')
